469b0f50a2bf8a30255a6f4eb083207f0014970a
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-07-17 00:37:25 +0530

    Using useState to handle different states and pages

--- src/Page/Dashboard/index.js
+++ src/Page/Dashboard/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Swal from "sweetalert2";
+
 import { studentsData } from "../../data";
+
 import Header from "./Header";
 import Add from "./Add";
 import Edit from "./Edit";
 import List from "./List";
+
 function Dashboard() {
+  const [students, setStudents] = useState(studentsData);
+  const [isAdding, setIsAdding] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   return <div> Dashboard</div>;
 }
 
