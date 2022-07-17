3d7cab8c94f9d2d9f737ff66a435d73d8e20ddbd
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-07-17 19:41:16 +0530

    Displaying the already stored data of students

--- src/Page/Dashboard/List.js
+++ src/Page/Dashboard/List.js
@@ -1,7 +1,59 @@
 import React from "react";
 
 function List({ students, handleEdit, handleDelete }) {
-  return <div>List</div>;
+  return (
+    <div className="contain-table">
+      <table className="striped-table">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Branch</th>
+            <th>DOB</th>
+            <th colSpan={2} className="text-center">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.length > 0 ? (
+            students.map((student, i) => (
+              <tr key={student.id}>
+                <td>{i + 1}</td>
+                <td>{student.firstName}</td>
+                <td>{student.lastName}</td>
+                <td>{student.email}</td>
+                <td>{student.branch}</td>
+                <td>{student.dob} </td>
+                <td className="text-right">
+                  <button
+                    onClick={() => handleEdit(student.id)}
+                    className="button muted-button"
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td className="text-left">
+                  <button
+                    onClick={() => handleDelete(student.id)}
+                    className="button muted-button"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={7}>No students</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default List;
