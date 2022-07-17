fa9da5d795e44fc95f428128678db75262a389a1
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-07-17 20:02:05 +0530

    Creating a add form for adding student details

--- src/Page/Dashboard/Add.js
+++ src/Page/Dashboard/Add.js
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import Swal from "sweetalert2";
 
-function Add() {
-  return <div>Add</div>;
+function Add({ students, setStudents, setIsAdding }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [branch, setBranch] = useState("");
+  const [dob, setDob] = useState("");
+
+  const textInput = useRef(null);
+
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (!firstName || !lastName || !email || !branch || !dob) {
+      return Swal.fire({
+        icon: "error",
+        title: "Error!",
+        text: "All fields are required.",
+        showConfirmButton: true,
+      });
+    }
+
+    const id = students.length + 1;
+    const newStudent = {
+      id,
+      firstName,
+      lastName,
+      email,
+      branch,
+      dob,
+    };
+    students.push(newStudent);
+    setStudents(students);
+    setIsAdding(false);
+
+    Swal.fire({
+      icon: "success",
+      title: "Added!",
+      text: `${firstName} ${lastName}'s data has been Added.`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  return (
+    <div className="small-container">
+      <form onSubmit={handleAdd}>
+        <h1>Add Student</h1>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          type="text"
+          ref={textInput}
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="branch">branch </label>
+        <input
+          id="branch"
+          type="text"
+          name="branch"
+          value={branch}
+          onChange={(e) => setBranch(e.target.value)}
+        />
+        <label htmlFor="dob">dob</label>
+        <input
+          id="dob"
+          type="date"
+          name="dob"
+          value={dob}
+          onChange={(e) => setDob(e.target.value)}
+        />
+        <div style={{ marginTop: "30px" }}>
+          <input type="submit" value="Add" />
+          <input
+            style={{ marginLeft: "12px" }}
+            className="muted-button"
+            type="button"
+            value="Cancel"
+            onClick={() => setIsAdding(false)}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Add;
